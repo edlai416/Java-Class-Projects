c9ba30b2d738863cecf8bde7012692cf8f2ea06c
edlai416 <31459581+edlai416@users.noreply.github.com>
2019-08-08 13:25:03 -0700

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class Tester here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Random;
+public class Tester
+{
+    // instance variables - replace the example below with your own
+    public Random x = new Random();
+
+    /**
+     * Constructor for objects of class Tester
+     */
+    public static void main()
+    {
+        while(true)
+        {
+        System.out.println("YEET");
+    }
+    }
+
+  
+   
+}
